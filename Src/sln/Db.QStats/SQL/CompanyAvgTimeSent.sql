779bec189cf093169e609b441bc034eb6b0643f0
AlexPi <alex.pigida@outlook.com>
2022-11-13 09:12:12 -0500

    88

--- Src/sln/Db.QStats/SQL/CompanyAvgTimeSent.sql
+++ Src/sln/Db.QStats/SQL/CompanyAvgTimeSent.sql
@@ -1,8 +1,6 @@
 use QStatsRls
 go
-
-SELECT     COUNT(*) AS Cnt FROM        EHist WHERE     (EHist.SentOn IS NOT NULL)
-
+-- SELECT     COUNT(*) AS Cnt FROM        EHist WHERE     (EHist.SentOn IS NOT NULL)
 SELECT      EMail.Company, COUNT(*) AS TtlSent
 , format(24*AVG(CAST(CAST(CONVERT(varCHAR, EHist.SentOn, 108) AS datetime) AS float)), 'N1') AS [Avg Sent On]
 , format(24*( Max(CAST(CAST(CONVERT(varCHAR, EHist.SentOn, 108) AS datetime) AS float))              -      Min(CAST(CAST(CONVERT(varCHAR, EHist.SentOn, 108) AS datetime) AS float))            ), 'N1') AS [Work Day (hr)]
@@ -16,5 +14,5 @@ FROM        EHist INNER JOIN
             EMail ON EHist.EMailID = EMail.ID
 WHERE     (CONVERT(varCHAR, EHist.SentOn, 108) <> '00:00:00')
 GROUP BY EMail.Company
-HAVING     (COUNT(*) > 1)
-ORDER BY 8 Desc, EMail.Company
+HAVING     (COUNT(*) > 0)
+ORDER BY 1, 2 desc
