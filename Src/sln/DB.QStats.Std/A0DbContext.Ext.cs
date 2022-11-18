f08663081871dbd011a1afc877638c286b39a264
AlexPi <alex.pigida@outlook.com>
2022-11-18 10:09:04 -0500

    pau

--- Src/sln/DB.QStats.Std/A0DbContext.Ext.cs
+++ Src/sln/DB.QStats.Std/A0DbContext.Ext.cs
@@ -2,16 +2,19 @@
 
 public partial class Email : INotifyPropertyChanged
 {
-  [NotMapped] public int? Ttl_Sent { get; set; }
-  [NotMapped] public int? Ttl_Rcvd { get; set; }
-  [NotMapped] public DateTime? LastSent { get; set; }
-  [NotMapped] public DateTime? LastRcvd { get; set; }
+  [NotMapped] public int? Ttl_Sent { get => ttl_Sent; set { if (value != ttl_Sent) { ttl_Sent = value; OnPropertyChanged(); } } }
+  [NotMapped] public int? Ttl_Rcvd { get => ttl_Rcvd; set { if (value != ttl_Rcvd) { ttl_Rcvd = value; OnPropertyChanged(); } } }
+  [NotMapped] public DateTime? LastSent { get => lastSent; set { if (value != lastSent) { lastSent = value; OnPropertyChanged(); } } }
+  [NotMapped] public DateTime? LastRcvd { get => lastRcvd; set { if (value != lastRcvd) { lastRcvd = value; OnPropertyChanged(); } } }
   [NotMapped] public string? Country { get => _country; set { if (value != _country) { _country = value; OnPropertyChanged(); } } }
 
   string? _country, _pbr;
+  int? ttl_Sent;
+  int? ttl_Rcvd;
+  DateTime? lastSent;
+  DateTime? lastRcvd;
 
   //[ObservableProperty] string? country;
-
 
   public event PropertyChangedEventHandler? PropertyChanged;
   protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
